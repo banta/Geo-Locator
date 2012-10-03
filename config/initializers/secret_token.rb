1caa6b437b1f6cb3f55e23327f54c8197bf93a21
Bernard Banta <banta.bernard@gmail.com>
2012-10-03 10:57:46 +0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoLocator::Application.config.secret_token = '325733924256a17e64e0e1cf8928710d7f6c7145a4fa4392ca7516da49cba01f3f535adb2c4d53ad5cb918233936eaf144538a1ac2f8d7db8a32558f99182933'
